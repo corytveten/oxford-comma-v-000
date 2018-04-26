ec8265c0d9060e881dd6527623e3ef03bb28195c
corytveten <corytveten@gmail.com>
2018-04-26 00:39:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array == 1
+  if array.size == 1
   return[0]
-elsif array == 2
+elsif array.size == 2
   return array.join(" and ")
 else
   return array[0..-2].join(", ") + ", and " + array[-1]
